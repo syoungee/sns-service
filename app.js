1148719bb17d206c553eebd9a8a1828379ff07b0
syoungee <sunyoungah69@gmail.com>
2022-07-22 09:48:39 +0900

    초기 세팅

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const config = require('./config');
+const express = require("express");
+const config = require("./config");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello welcome to my sns-service!🤍')
-})
-
 app.listen(port, () => {
-  console.log(`Express is running on port ${port}`)
-})
+  console.log(`Express is running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello welcome to my sns-service!🤍");
+});
