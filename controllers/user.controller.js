abdaf1e6f297eb3a928e8cd4c09d1db24fbe6279
syoungee <sunyoungah69@gmail.com>
2022-07-24 16:23:54 +0900

    로그인 api 구현

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,6 +12,18 @@ class UserController {
       console.log(err);
     }
   }
+
+  static async signin(req, res) {
+    console.log("signin 함수 호출", req.body);
+    const { email, password } = req.body;
+    try {
+      const userInfo = await UserService.login(email, password);
+      res.status(201).send({ message: `안녕하세요 ${userInfo.user_name}님 로그인되었습니다!` });
+    } catch (err) {
+      console.log("signin 함수 에러");
+      console.log(err);
+    }
+  }
 }
 
 module.exports = UserController;
--- routes/user.router.js
+++ routes/user.router.js
@@ -4,5 +4,8 @@ const UserController = require("../controllers/user.controller");
 
 // 회원 가입 요청
 router.post("/", UserController.signup);
+// 로그인
+router.get("/login", UserController.signin);
+// TODO: 로그아웃
 
 module.exports = router;
