a55def3043b35e40db22c59f4c735e2ab8148c4e
syoungee <sunyoungah69@gmail.com>
2022-07-25 12:47:34 +0900

    게시글 만들기 api 구현

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const userRouter = require("./userRouter");
+const loginRouter = require("./loginRouter");
+const postRouter = require("./postRouter");
 
 router.use("/user", userRouter);
+router.use("/login", loginRouter);
+router.use("/post", postRouter);
 
 module.exports = router;
