40a24d35e7fa9c7940074c9a589c7e5896f927d6
syoungee <sunyoungah69@gmail.com>
2022-08-02 21:58:23 +0900

    Update: docker file

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM node:14
 
 WORKDIR /
-COPY package.json .
+COPY package*.json .
 RUN npm install
+RUN npm install -g nodemon
+
 COPY . .
 CMD npm start
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const LoginController = require("../controllers/loginController");
 
+
+
 // 로그인
 router.get("/", LoginController.signin);
 // TODO: 로그아웃
